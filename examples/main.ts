b958465e26857b0de4bc68d62cb5836efc368d9a
TangSY <337828932@qq.com>
2022-02-28 19:31:30 +0800

    feat: 去除vue-ba

--- examples/main.ts
+++ examples/main.ts
@@ -2,14 +2,10 @@ import Vue, { createApp } from "vue";
 import App from "./App.vue";
 // 导入组件库
 import vueHashCalendar from "../packages/index";
-// 引入百度统计插件
-import ba from "vue-ba";
 
 const app = createApp(App);
 
 // 注册组件库
 app.use(vueHashCalendar);
-
-// app.use(ba, "b0668f30d62e1597bdb36d05edea8960");
 
 app.mount("#app");
