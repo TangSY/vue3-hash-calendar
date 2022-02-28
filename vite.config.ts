35039677e1d8e45f7808c657938334553468e802
TangSY <337828932@qq.com>
2022-02-28 19:30:17 +0800

    core: 修改打包配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import DefineOptions from 'unplugin-vue-define-options/vite'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import DefineOptions from "unplugin-vue-define-options/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), DefineOptions()]
-})
+  base: "./",
+  plugins: [vue(), DefineOptions()],
+});
