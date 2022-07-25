2f0df3220d0b81392dcd5614fbd0ccaa38af9ccb
TangSY <337828932@qq.com>
2022-07-25 23:17:30 +0800

    chore: update peerDependencies

--- examples/main.ts
+++ examples/main.ts
@@ -4,7 +4,8 @@ import App from "./App.vue";
 // import App from "./lunar/LunarDemo.vue";
 // 导入组件库
 import vueHashCalendar from "../packages/index";
-
+// import vueHashCalendar from "vue3-hash-calendar";
+// import "vue3-hash-calendar/lib/style.css";
 const app = createApp(App);
 
 // 注册组件库
