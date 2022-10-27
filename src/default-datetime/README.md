444e594fe88c8bb6167f8ec70130a2d57a262575
TangSY <337828932@qq.com>
2022-10-28 00:24:13 +0800

    docs: 添加 default-datetime 属性文档

---
+++ src/default-datetime/README.md
@@ -0,0 +1,17 @@
+# default-datetime
+
+## 当前时间
+
+### 代码演示
+
+```html
+<vue-hash-calendar :default-datetime="new Date()" />
+```
+
+## 2022/11/05
+
+### 代码演示
+
+```html
+<vue-hash-calendar :default-datetime="new Date(2022/11/05)" />
+```
---
+++ src/default-datetime/demo/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1 class="demo-title">当前时间</h1>
+  <vue-hash-calendar :default-datetime="new Date()" />
+
+  <h1 class="demo-title">2022/11/05</h1>
+  <vue-hash-calendar :default-datetime="new Date(2022 / 11 / 05)" />
+</template>
+
+<script setup>
+import VueHashCalendar from '../../calendar';
+</script>
+
+<style>
+.demo-title {
+  margin: 24px;
+}
+</style>
