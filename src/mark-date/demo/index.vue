f4c3efa2e11dfdfbff4cf7ef35c722ee5bb81a86
TangSY <337828932@qq.com>
2022-10-29 17:29:56 +0800

    docs: 完善 mark-datet 属性文档

--- src/mark-date/demo/index.vue
+++ src/mark-date/demo/index.vue
@@ -1,9 +1,68 @@
 <template>
-  <h1 class="hash-demo-title">当前时间</h1>
-  <vue-hash-calendar :default-datetime="new Date()" />
+  <h1 class="hash-demo-title">默认</h1>
+  <vue-hash-calendar
+    :mark-date="[
+      '2022/11/11',
+      '2022/12/11',
+      '2023/01/11',
+      '2023/02/11',
+      '2023/03/11',
+      '2023/04/11',
+      '2023/05/11',
+    ]"
+  />
 
-  <h1 class="hash-demo-title">2022/11/05 22:22:22</h1>
-  <vue-hash-calendar :default-datetime="new Date('2022/11/05  22:22:22')" />
+  <h1 class="hash-demo-title">自定义颜色</h1>
+  <vue-hash-calendar
+    :mark-date="[
+      {
+        color: '#f00',
+        date: [
+          '2022/11/11',
+          '2022/12/11',
+          '2023/01/11',
+          '2023/02/11',
+          '2023/03/11',
+          '2023/04/11',
+          '2023/05/11',
+        ],
+      },
+    ]"
+  />
+
+  <h1 class="hash-demo-title">自定义标记类型</h1>
+  <vue-hash-calendar
+    :mark-date="[
+      {
+        type: 'dot',
+        date: [
+          '2022/11/11',
+          '2022/12/11',
+          '2023/01/11',
+          '2023/02/11',
+          '2023/03/11',
+          '2023/04/11',
+          '2023/05/11',
+        ],
+      },
+    ]"
+  />
+
+  <h1 class="hash-demo-title">交叉使用</h1>
+  <vue-hash-calendar
+    :mark-date="[
+      {
+        color: '#f00',
+        type: 'dot',
+        date: ['2022/11/11', '2022/12/11', '2023/04/11'],
+      },
+      {
+        type: 'circle',
+        date: ['2023/01/11', '2023/02/11', '2023/03/11'],
+      },
+      '2023/05/11',
+    ]"
+  />
 </template>
 
 <script setup>
