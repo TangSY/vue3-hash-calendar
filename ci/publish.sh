7f78fb45942b2e689d72ff4b912214e0bb8d03ab
TangSY <337828932@qq.com>
2022-11-09 10:00:50 +0800

    chore: update publish.sh

--- ci/publish.sh
+++ ci/publish.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-cat > .npmrc <<-EOF
-> //registry.npmjs.org/:_authToken=${NPM_TOKEN}
-> registry=https://registry.npmjs.org/
-> home=https://www.npmjs.org
-> EOF
+
 yarn build
+cat > .npmrc <<-EOF
+//registry.npmjs.org/:_authToken=${NPM_TOKEN}
+registry=https://registry.npmjs.org/
+home=https://www.npmjs.org
+EOF
 npm publish
